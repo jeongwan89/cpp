fff09ec3ff89b7eb2973b3c8eee0809f7fc8ccfd
jeongwan89 <jeongwan89@mac.com>
2024-07-08 13:33:38 +0900

    operator++

--- prac/STL/Const/const.cpp
+++ prac/STL/Const/const.cpp
@@ -11,23 +11,33 @@ public:
     void SetX(int _x) { x = _x;}
     void SetY(int _y) { y = _y;}
     void Print() const { std::cout << x << ", " << y << std::endl;}
+    const Point & operator ++ () // 전위 ++
+    {
+        ++x;
+        ++y;
+        return * this;
+    }
+    const Point operator ++ (int) // 후위 ++
+    {
+        Point tmp = * this;
+        ++ *this;
+        return tmp;
+    }
 };
 
 int main()
 {
-    const Point p1(0,0);
+    Point p1(2, 3);
     Point p2(2, 3);
+    Point result;
 
+    result = ++ p1; // p1.operator ++ ();
     p1.Print();
+    result.Print();
+
+    result = p2 ++; // p2.operator ++ (0);
     p2.Print();
-
-    std::cout << "p1: " << p1.GetX() << ", " << p1.GetY() << std::endl;
-    std::cout << "p2: " << p2.GetX() << ", " << p2.GetY() << std::endl;
-
-    p2.SetX(5);
-    p2.SetY(5);
-
-    p2.Print();
+    result.Print();
 
     return 0;
 }
