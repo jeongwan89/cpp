730add35f370ba4b73e731831a966aa05a5ff83d
jeongwan89 <jeongwan89@mac.com>
2024-07-08 17:09:49 +0900

    함수포인터

--- prac/pointer/fptr.cpp
+++ prac/pointer/fptr.cpp
@@ -13,9 +13,26 @@ void calc(int (*fptr)(int, int), int lv, int rv)
     cout << fptr(lv, rv) << endl << endl;
 }
 
+void Print(int n)
+{
+    cout << "정수: " << n << endl;
+
+}
 int main()
 {
     calc(sum, 1, 5); //드디어 매개변수를 통해 함수를 전달할 수 있습니다.
+
+    void (*pf)(int);
+    pf = Print;
+
+    Print(10);  // 1. 함수호출
+    pf(10);     // 2. 포인터를 이용한 함수 호출, 첫 번째 방법
+    (*pf)(10);  // 3. 포인터를 이용한 함수 호출, 두 번째 방법
+
+    cout << endl;
+    cout << Print << endl;
+    cout << pf << endl;
+    cout << *pf << endl;
 
     return 0;
 }
